43ed634b4b20264e66563044aac21eed95916cc5
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-06-17 11:09:03 +0200

    first and last

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from 'redux/todoSlice';
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+import { nanoid } from 'nanoid';
 
-const SearchForm = ({ onSubmit }) => {
-  const [query, setQuery] = useState('')
-
+const SearchForm = () => {
+  const [query, setQuery] = useState('');
+  const dispatch = useDispatch();
   const handleInput = e => {
-    setQuery(e.currentTarget.value)
-
+    setQuery(e.currentTarget.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    onSubmit(query);
-    setQuery('')
+    const todo = {
+      id: nanoid(),
+      text: query,
+    };
+    dispatch(addTodo(todo));
+    setQuery('');
   };
   return (
     <SearchFormStyled onSubmit={handleSubmit}>
@@ -31,6 +36,6 @@ const SearchForm = ({ onSubmit }) => {
       />
     </SearchFormStyled>
   );
-}
+};
 
-export default SearchForm
+export default SearchForm;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import 'modern-normalize';
-
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import App from './components/App/App';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <Global styles={GlobalStyles} />
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+    ,
   </React.StrictMode>
 );
